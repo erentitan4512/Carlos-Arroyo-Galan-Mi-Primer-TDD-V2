121c03354845643ce81be1f7bb5d1f3389d5a3b2
erentitan4512 <116826112+erentitan4512@users.noreply.github.com>
2023-06-11 20:01:13 +0200

    Add files via upload

---
+++ TestCoches/src/Coche.java
@@ -0,0 +1,13 @@
+
+public class Coche {
+
+	public int velocidad;
+
+    public void acelerar(int aceleración) {
+        velocidad += aceleración;
+    }
+    public void decelerar(int deceleración) {
+        velocidad -= deceleración;
+        if (velocidad <0) velocidad = 0;
+    }
+}
